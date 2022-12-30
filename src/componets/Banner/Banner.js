82f9f2bef1b819762c23141f82f0cf94129948b5
Leandro Farias <leandrofarias93@gmail.com>
2022-12-30 20:29:57 -0300

    Entrega Final

---
+++ src/componets/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-container'>
+        <h1>Librería Rosendo</h1>
+        <p>Los mejores libros en un solo lugar!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
